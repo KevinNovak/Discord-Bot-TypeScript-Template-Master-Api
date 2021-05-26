185b2cf1d84c34f0061ed4c21a2417fba3397aee
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-05-25 20:43:50 -0400

    Autoformat

--- src/caches/cluster-cache.ts
+++ src/caches/cluster-cache.ts
@@ -8,7 +8,10 @@ export class ClusterCache {
     private static cache: Cache = flatCache.load('clusters', path.join(__dirname, '../../.cache'));
 
     public static getAll(): Cluster[] {
-        return this.cache.keys().map(key => this.cache.getKey(key) as Cluster).sort((a, b) => Math.min(...a.allocatedShardIds) - Math.min(...b.allocatedShardIds));
+        return this.cache
+            .keys()
+            .map(key => this.cache.getKey(key) as Cluster)
+            .sort((a, b) => Math.min(...a.allocatedShardIds) - Math.min(...b.allocatedShardIds));
     }
 
     public static get(clusterId: string): Cluster {
@@ -28,22 +31,22 @@ export class ClusterCache {
     }
 
     public static online(): Cluster[] {
-        return this.getAll().filter(cluster => cluster.status === ClusterStatus.ONLINE)
+        return this.getAll().filter(cluster => cluster.status === ClusterStatus.ONLINE);
     }
 
     public static shardIds(): number[] {
-        return [...new Set(this.getAll().flatMap(cluster => cluster.allocatedShardIds))].sort()
+        return [...new Set(this.getAll().flatMap(cluster => cluster.allocatedShardIds))].sort();
     }
 
     public static maxShardId(): number {
         let shardIds = this.shardIds();
         if (shardIds.length > 0) {
-            return Math.max(...shardIds)
+            return Math.max(...shardIds);
         }
     }
 
     public static totalShards(): number {
-        let maxShardId = this.maxShardId()
+        let maxShardId = this.maxShardId();
         return maxShardId ? maxShardId + 1 : 0;
     }
 
@@ -52,10 +55,10 @@ export class ClusterCache {
             this.getAll()
                 .filter(cluster => !!cluster.getGuilds)
                 .flatMap(cluster => cluster.getGuilds.guilds)
-        ).size
+        ).size;
     }
 
     public static serversPerShard(): number {
-        return Math.round(this.serverCount() / (this.totalShards() || 1))
+        return Math.round(this.serverCount() / (this.totalShards() || 1));
     }
 }
