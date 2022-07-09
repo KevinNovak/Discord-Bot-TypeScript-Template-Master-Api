62f2b3898fb54fe36827c7277f28504090adcc21
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-07-09 11:50:25 -0400

    Use pino for logging

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,68 +1,71 @@
 import { Response } from 'node-fetch';
+import { createRequire } from 'node:module';
+import pino from 'pino';
+
+const require = createRequire(import.meta.url);
+let Config = require('../../config/config.json');
+
+let logger = pino(
+    {
+        formatters: {
+            level: label => {
+                return { level: label };
+            },
+        },
+    },
+    Config.logging.pretty
+        ? pino.transport({
+              target: 'pino-pretty',
+              options: {
+                  colorize: true,
+                  ignore: 'pid,hostname',
+                  translateTime: 'yyyy-mm-dd HH:MM:ss.l',
+              },
+          })
+        : undefined
+);
 
 export class Logger {
-    private static shardTag: string;
-
-    public static info(message: string): void {
-        let log = '[Info]';
-        if (this.shardTag) {
-            log += ' ' + this.shardTag;
-        }
-        log += ' ' + message;
-        console.log(log);
+    public static info(message: string, obj?: any): void {
+        obj ? logger.info(obj, message) : logger.info(message);
     }
 
-    public static warn(message: string): void {
-        let log = '[Warn]';
-        if (this.shardTag) {
-            log += ' ' + this.shardTag;
-        }
-        log += ' ' + message;
-        console.warn(log);
+    public static warn(message: string, obj?: any): void {
+        obj ? logger.warn(obj, message) : logger.warn(message);
     }
 
-    public static async error(message: string, error?: any): Promise<void> {
-        // Log custom error message
-        let log = '[Error]';
-        if (this.shardTag) {
-            log += ' ' + this.shardTag;
-        }
-        log += ' ' + message;
-        console.error(log);
-
-        // Log error object if exists
-        if (!error) {
+    public static async error(message: string, obj?: any): Promise<void> {
+        // Log just a message if no error object
+        if (!obj) {
+            logger.error(message);
             return;
         }
 
-        switch (error.constructor) {
-            case Response: {
-                let res = error as Response;
-                let resText: string;
-                try {
-                    resText = await res.text();
-                } catch {
-                    // Ignore
-                }
-                console.error({
-                    path: res.url,
-                    statusCode: res.status,
-                    statusName: res.statusText,
-                    headers: res.headers.raw(),
+        // Otherwise log details about the error
+        if (typeof obj === 'string') {
+            logger
+                .child({
+                    message: obj,
+                })
+                .error(message);
+        } else if (obj instanceof Response) {
+            let resText: string;
+            try {
+                resText = await obj.text();
+            } catch {
+                // Ignore
+            }
+            logger
+                .child({
+                    path: obj.url,
+                    statusCode: obj.status,
+                    statusName: obj.statusText,
+                    headers: obj.headers.raw(),
                     body: resText,
-                });
-                break;
-            }
-            default: {
-                console.error(error);
-                break;
-            }
-        }
-    }
-
-    public static setShardId(shardId: number): void {
-        if (shardId > -1) {
-            this.shardTag = `[Shard ${shardId.toString()}]`;
+                })
+                .error(message);
+        } else {
+            logger.error(obj, message);
         }
     }
 }
